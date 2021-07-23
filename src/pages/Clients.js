81adeee6aa9a428e83fd02e541d9cf798ec19179
kelvinbass <kelbass17@gmail.com>
2021-07-23 18:05:38 -0300

    adicionado um background-image

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -5,8 +5,11 @@ import InputMask from 'react-input-mask'
 import Logo from '../assets/Vtex-logo.png'
 
 export default function SingIn() {
+   const [nome, setNome] = useState('')
    const [cpf, setCpf] = useState('')
+   const [email, setEmail] = useState('')
    const [telefone, setTelefone] = useState('')
+
 
    return (
       <div className="card-container">
@@ -14,16 +17,16 @@ export default function SingIn() {
             <img src={Logo} alt="Vtex-logo" className="logo-cadastro"></img>
             <h2>Cadastro de clientes</h2>
             <p>Preencha os dados corretamente.</p>
-            <label htmlFor="nome">Nome</label>
-            <InputMask className="camp" id="nome" type="text" placeholder="Digite seu nome completo"></InputMask>
-            <label htmlFor="cpf">CPF</label>
-            <InputMask value={cpf} onChange={(event) => setCpf(event.target.value)} className="camp" mask="999.999.999-99" id="cpf" type="text" placeholder="Digite seu cpf"></InputMask>
-            <label htmlFor="email">Email</label>
-            <InputMask className="camp" id="email" type="email" placeholder="Digite seu email"></InputMask>
-            <label htmlFor="telefone">Telefone</label>
-            <InputMask value={telefone} onChange={(event) => setTelefone(event.target.value)} mask="(99) 99999-9999" className="camp" id="telefone" type="tel" placeholder="Digite seu telefone com o DDD"></InputMask>
+            <label  className="label-client" htmlFor="nome">Nome</label>
+            <InputMask value={nome} onChange={(e)=> setNome(e.target.value)} className="input-client" id="nome" type="text" placeholder="Digite seu nome completo"></InputMask>
+            <label className="label-client"  htmlFor="cpf">CPF</label>
+            <InputMask value={cpf} onChange={(event) => setCpf(event.target.value)} className="input-client" mask="999.999.999-99" id="cpf" type="text" placeholder="Digite seu cpf"></InputMask>
+            <label className="label-client"  htmlFor="email">Email</label>
+            <InputMask value={email} onChange={(e) => setEmail(e.target.value)} className="input-client" id="email" type="email" placeholder="Digite seu email"></InputMask>
+            <label className="label-client"  htmlFor="telefone">Telefone</label>
+            <InputMask value={telefone} onChange={(event) => setTelefone(event.target.value)} mask="(99) 9 9999-9999" className="input-client" id="telefone" type="tel" placeholder="Digite seu telefone com o DDD"></InputMask>
 
-            <button type="submit" className="card-button-cadastrar">Cadastrar cliente</button>
+            <button type="submit" className="card-button-cadastrar-client">Cadastrar cliente</button>
             <Link className="card-button-voltar" to="/"> &lt; Voltar</Link>
 
             
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,5 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import InputMask from 'react-input-mask'
+import '../style.css'
 
-export default function Products() {
-   return <h1>PRODUCTS</h1>
+import Logo from '../assets/Vtex-logo.png'
+
+export default function SingIn() {
+
+   const [produto, setProduto] = useState('')
+   const [options, setOptions] = useState('')
+   const [descricao, setDescricao] = useState('')
+   const [preco, setPreco] = useState('')
+   const [quantidade, setQuantidade] = useState('')
+
+   return (
+      <div className="card-container">
+         <form className="card-content">
+            <img src={Logo} alt="Vtex-logo" className="logo-cadastro"></img>
+            <h2>Cadastro de produtos</h2>
+            <p>Preencha os dados corretamente.</p>
+            <label className="label-product"  htmlFor="produto">Produto</label>
+            <input value={produto} onChange={(e)=> setProduto(e.target.value)} className="input-product" id="produto" type="text" placeholder="Digite qual o é o produto"></input>
+
+            <label className="label-product" >Categoria</label>
+            <select value={options} onChange={(e) => setOptions(e.target.value)} className="input-product">
+               <option value="selecione">Selecione</option>
+               <option value="notebooks">Notebooks</option>
+               <option value="smartphones">Smartphones</option>
+               <option value="acessorios">Acessórios</option>
+               <option value="outros">Outros</option>
+            </select>
+
+            <label  className="label-product" htmlFor="descricao">Descrição</label>
+            <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} className="input-product" id="descricao" type="text" placeholder="Especificações básicas do produto"></textarea>
+            <label  className="label-product" htmlFor="preco">Preço</label>
+            <InputMask value={preco} onChange={(e) => setPreco(e.target.value)} className="input-product" id="preco" type="text" placeholder="Digite o valor do produto"></InputMask>
+            <label  className="label-product" htmlFor="quantidade">Quantidade em estoque</label>
+            <input value={quantidade} onChange={(e) => setQuantidade(e.target.value)} className="input-product" id="quantidade" type="text" placeholder="Digite a quantidade disponivel"></input>
+
+            <button type="submit" className="card-button-cadastrar-product">Cadastrar produto</button>
+            <Link className="card-button-voltar" to="/"> &lt; Voltar</Link>
+
+
+         </form>
+
+      </div>
+   )
 }
